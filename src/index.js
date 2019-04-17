3520d63cae9c79cbd90524ea62c9b0be9b667c92
Sergio Fernando Maita Chamba <smaita@ioet.com>
2019-04-17 16:04:40 -0500

    Add first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { Provider } from 'react-redux';
+import { rootEpic } from './epics';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const epicMiddleware = createEpicMiddleware();
+const store = createStore(rootReducer,  compose(
+    applyMiddleware(epicMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+epicMiddleware.run(rootEpic);
+
+const appWithProvider = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(appWithProvider, document.getElementById('root'));
